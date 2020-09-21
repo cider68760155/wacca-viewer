16f17d67a4023f324d27d17d47b404846f6fbb70
cider68760155 <example@example.com>
2020-09-21 22:30:57 +0900

    +のつく曲の表記方法を.5に変更

--- crawl_song.py
+++ crawl_song.py
@@ -19,14 +19,14 @@ try:
     soup = BeautifulSoup(driver.page_source, 'html.parser')
     category = [a.text for a in soup.find_all(class_='data_cat')]
     title = [a.text for a in soup.find_all(class_='data_name')]
-    level = [[a.text.replace(' ', '') for a in levels.find_all(
+    level = [[a.text.replace(' ', '').replace('+', '.5') for a in levels.find_all(
         class_='level_value')] for levels in soup.find_all(class_='level_list')]
     ret = [{} for i in range(num_song)]
     for i in range(num_song):
         ret[i]['title'] = title[i]
         ret[i]['category'] = category[i]
         for j, lv in enumerate(level[i]):
-            ret[i]['level_' + DIFFICULTY[j]] = lv
+            ret[i]['level_' + DIFFICULTY[j]] = float(lv)
     print(ret)
     with open('./docs/data/songs.json', 'w') as f:
         json.dump(ret, f, ensure_ascii=False)
